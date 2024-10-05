9a38504f56e4e0354eec68a85d28b20d74385010
Shaun MR <shaunmr009@gmail.com>
2024-10-05 18:33:07 +0530

    checkV2

---
+++ questions.js
@@ -0,0 +1,62 @@
+let questions = [
+  {
+    numb: 1,
+    question:
+      " What is the primary objective of the PACE satellite in ocean monitoring?",
+    answer: "c. To monitor phytoplankton and ocean color",
+    options: [
+      `a.  To study tectonic plate movements`,
+      `b.  To measure sea surface temperatures`,
+      `c. To monitor phytoplankton and ocean color`,
+      `d. To track marine life migration patterns`,
+    ],
+  },
+  {
+    numb: 2,
+    question:
+      "How does the PACE satellite help in studying the Earth's atmosphere?",
+    answer: "b. By measuring aerosol particles and cloud cover",
+    options: [
+      `a. By tracking thunderstorms and hurricanes`,
+      `b. By measuring aerosol particles and cloud cover`,
+      `c. By detecting volcanic eruptions`,
+      `d. By predicting weather patterns`,
+    ],
+  },
+  {
+    numb: 3,
+    question:
+      "Why is the measurement of ocean color important for the PACE satellite's mission?",
+    answer: "d. It provides insights into phytoplankton populations",
+    options: [
+      `a. It indicates ocean depth`,
+      `b. It reveals the presence of pollutants`,
+      `c.  It helps track ocean currents`,
+      `d. It provides insights into phytoplankton populations`,
+    ],
+  },
+  {
+    numb: 4,
+    question:
+      "In what way does the PACE satellite contribute to climate change research?",
+    answer:
+      "c.  By measuring how aerosols and clouds impact global temperatures",
+    options: [
+      `By monitoring volcanic ash clouds`,
+      `b. By analyzing changes in ice caps`,
+      `c.  By measuring how aerosols and clouds impact global temperatures`,
+      `d. By tracking forest fires`,
+    ],
+  },
+  {
+    numb: 5,
+    question: " How does PACE improve our understanding of air quality?",
+    answer: "c. By measuring aerosol particles that affect air quality",
+    options: [
+      `a.By detecting carbon emissions from vehicles`,
+      `b.By monitoring deforestation`,
+      `c. By measuring aerosol particles that affect air quality`,
+      `d. By tracking CO2 levels in the upper atmosphere`,
+    ],
+  },
+];
